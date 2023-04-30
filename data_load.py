62dad13e40426ac46a1effcb1dee9a27ae1d7005
North_Samurai <59742048+omega2705@users.noreply.github.com>
2023-04-30 21:56:12 +0300

    доработан дата лоадер,добавлены два датасета по биткоину с yahoo

--- data_load.py
+++ data_load.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
+
 def date_(data,date=False):
     """
         переработка даты
-        :param data: данные
-        :param date: если = True о дата будет преобразована в три столбца, если = False то дата уберается, по умолчанию = False        :return: возвращает датасет с разделенным категгориальным фактором
+    :param data: данные
+    :param date: если = True о дата будет преобразована в три столбца, если = False то дата уберается, по умолчанию = False        :return: возвращает датасет с разделенным категгориальным фактором
     """
     if not (date):
         data = data.drop(['Date'], axis=1)
@@ -15,18 +16,38 @@ def date_(data,date=False):
         data = data.drop(['Date'], axis=1)
     return data
 
-def data_proc(data_loc,date=False,cat=[]):
+def data_drop(data,drop_col = []):
+    """
+        функия удаления нужных столбцов
+    :param drop_col: нужно передать названима нужных столбцов в массивв�
+    :return: возвращает датасет с удаленными столбцами
+    """
+    if drop_col != []:
+        data = data.drop(drop_col, axis=1)
+    return data
+
+def cat_(data,cat):
+    """
+        переработка категориальных факторов
+    :param data: данные в виде датафрейма pandas
+    :param cat: столбцы с категориями для переработки в числовые факторы
+    :return: датафрейм с без категариальных факторов
+    """
+    if  cat != []:
+        data = pd.get_dummies(data, columns=cat,dtype=float)
+    return data
+
+def data_proc(data_loc,date=False,cat=[],drop = []):
     """
         переработка данных
     :param data_loc: расположение данных
     :param date: если = True о дата будет преобразована в три столбца, если = False то дата уберается, по умолчанию = False
     :param cat: нужно передать название категориальных факторов в массиве, по умолчанию = []
-    :return: возвращает датасет с разделенным категгориальным фактором
+    :param drop: нужно передать названия нужных столбцов в массиве для удаления, по умолчанию = []
+    :return: возвращает переработанный датасет
     """
     data = pd.read_csv(data_loc)
-    if cat ==  []:
-        return date_(data,date)
-    else:
-        data = date_(data,date)
-        data = pd.get_dummies(data, columns=cat,dtype=float)
+    data = date_(data,date)
+    data = data_drop(data,drop)
+    data = cat_(data,cat)
     return data
