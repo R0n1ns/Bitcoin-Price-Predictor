659ce6d251f922fac69e782adb089d4c58317a04
North_Samurai <59742048+omega2705@users.noreply.github.com>
2023-04-30 15:29:49 +0300

    добавлена дополнительная обработка данных для работы с датой

--- data_load.py
+++ data_load.py
@@ -1,21 +1,32 @@
 import pandas as pd
+
+def date_(data,date=False):
+    """
+        переработка даты
+        :param data: данные
+        :param date: если = True о дата будет преобразована в три столбца, если = False то дата уберается, по умолчанию = False        :return: возвращает датасет с разделенным категгориальным фактором
+    """
+    if not (date):
+        data = data.drop(['Date'], axis=1)
+    else:
+        data["Day"] = [int(i.split("-")[2]) for i in data["Date"]]
+        data["Month"] = [int(i.split("-")[1]) for i in data["Date"]]
+        data["Year"] = [int(i.split("-")[0]) for i in data["Date"]]
+        data = data.drop(['Date'], axis=1)
+    return data
+
 def data_proc(data_loc,date=False,cat=[]):
     """
-    переработка данных
+        переработка данных
     :param data_loc: расположение данных
-    :param date: если = True о дата будет оставлено, если = False то дата уберается, по умолчанию = False
+    :param date: если = True о дата будет преобразована в три столбца, если = False то дата уберается, по умолчанию = False
     :param cat: нужно передать название категориальных факторов в массиве, по умолчанию = []
     :return: возвращает датасет с разделенным категгориальным фактором
     """
     data = pd.read_csv(data_loc)
     if cat ==  []:
-        if not(date):
-            data = data.drop(['Date'], axis=1)
-        data = pd.get_dummies(data, columns=["Value_Classification"])
-        return data
+        return date_(data,date)
     else:
-        if not(date):
-            data = data.drop(['Date'], axis=1)
+        data = date_(data,date)
         data = pd.get_dummies(data, columns=cat,dtype=float)
-
-        return data
+    return data
